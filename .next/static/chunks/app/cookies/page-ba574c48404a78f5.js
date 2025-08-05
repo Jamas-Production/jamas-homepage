1c803bbb5126aef12e5cd47430dd02306af74ed1
root <root@v42324.datalix.de>
2025-08-05 21:29:32 +0000

    Remove node_modules and .next from repo

--- .next/static/chunks/app/cookies/page-ba574c48404a78f5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[66,674,824,846,874,877,944],{9506:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[493,441,684,358],()=>s(9506)),_N_E=e.O()}]);
